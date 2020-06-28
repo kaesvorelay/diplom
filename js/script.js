479a71bd11774c8b03b72219fe6d882b244d0cfe
kaesvore <kaes_vorelay@icloud.com>
2020-06-28 22:18:57 +0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,51 +1,49 @@
 (function () {
-    const burger = document.querySelector('.toggle');
-    const nav = document.querySelector('.menu__list');
-    burger.addEventListener('click', () => {
-        burger.classList.toggle('toggle--active');
-        nav.classList.toggle('menu__list--active');
-    })
+  const burger = document.querySelector(".toggle");
+  const nav = document.querySelector(".menu__list");
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("toggle--active");
+    nav.classList.toggle("menu__list--active");
+  });
 
-    const elements = document.querySelector('.elements');
-    window.onload = () => {
-        elements.classList.add('elements--active');
-    }
-}());
+  const elements = document.querySelector(".elements");
+  window.onload = () => {
+    elements.classList.add("elements--active");
+  };
+})();
 
-new Swiper('.slider', {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    breakpoints: {
-        768: {
-            slidesPerView: 2
-        },
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 25
-        }
+new Swiper(".slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
     },
-    loop: true,
-    wrapperClass: 'slider__list',
-    slideClass: 'slider__item',
-    pagination: {
-        el: '.slider__pagination',
-        type: 'bullets',
-        bulletClass: 'paginator__item',
-        bulletActiveClass: 'paginator__item--active',
-        clickable: true
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 25,
     },
-    navigation: {
-        nextEl: '.slider__button--next',
-        prevEl: '.slider__button--prev',
-    },
-    a11y: {
-        prevSlideMessage: 'Предыдущий слайд',
-        nextSlideMessage: 'Следующий слайд',
-        paginationBulletMessage: 'Слудующий слайд {{index}}'
-    },
+  },
+  loop: true,
+  wrapperClass: "slider__list",
+  slideClass: "slider__item",
+  pagination: {
+    el: ".slider__pagination",
+    type: "bullets",
+    bulletClass: "paginator__item",
+    bulletActiveClass: "paginator__item--active",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".slider__button--next",
+    prevEl: ".slider__button--prev",
+  },
+  a11y: {
+    prevSlideMessage: "Предыдущий слайд",
+    nextSlideMessage: "Следующий слайд",
+    paginationBulletMessage: "Слудующий слайд {{index}}",
+  },
 });
-
-
 
 let modal = document.getElementsByClassName("first-modal")[0];
 let ButtonFirst = document.getElementsByClassName("open__first-modal")[0];
@@ -53,24 +51,24 @@ let ButtonSecond = document.getElementsByClassName("open__first-modal")[1];
 let hide = document.getElementsByClassName("close")[0];
 
 ButtonFirst.onclick = function () {
-    modal.style.display = "block";
-}
+  modal.style.display = "block";
+};
 
 ButtonSecond.onclick = function () {
-    modal.style.display = "block";
-}
+  modal.style.display = "block";
+};
 
 hide.onclick = function () {
-    modal.style.display = "none";
-}
+  modal.style.display = "none";
+};
 
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none"
-    } else if (event.target == modalTwo) {
-        modalTwo.style.display = "none"
-    }
-}
+  if (event.target == modal) {
+    modal.style.display = "none";
+  } else if (event.target == modalTwo) {
+    modalTwo.style.display = "none";
+  }
+};
 
 let modalTwo = document.getElementsByClassName("second-modal")[0];
 let ButtonFirstMod = document.getElementsByClassName("open-second__modal")[0];
@@ -79,99 +77,89 @@ let ButtonThirdMod = document.getElementsByClassName("open-second__modal")[2];
 let hideTwo = document.getElementsByClassName("close")[1];
 
 ButtonFirstMod.onclick = function () {
-    modalTwo.style.display = "block";
-}
+  modalTwo.style.display = "block";
+};
 
 ButtonSecondMod.onclick = function () {
-    modalTwo.style.display = "block";
-}
+  modalTwo.style.display = "block";
+};
 
 ButtonThirdMod.onclick = function () {
-    modalTwo.style.display = "block";
-}
+  modalTwo.style.display = "block";
+};
 
 hideTwo.onclick = function () {
-    modalTwo.style.display = "none";
-}
-
-
+  modalTwo.style.display = "none";
+};
 
 let input = document.querySelectorAll(`input[type = "tel"]`);
-let im = new Inputmask('+7 (999) 999-99-99');
+let im = new Inputmask("+7 (999) 999-99-99");
 im.mask(input);
 
+// let popupWrapperFirst = document.querySelector('.first-modal');
+// let popup = popupWrapperFirst.querySelector('.modal__content');
+// let popupForm = popup.querySelector('.modal__form');
+// let popupMessege = popup.querySelector('.modal__thanks');
 
-
-
-// function validateForms(selector, rules) {
-//     new window.JustValidate(selector, {
-//         rules: rules,
-//         submitHandler: function (form, values, ajax) {
-//             console.log(form);
-//             let formData = new FormData(form);
-//             fetch("mail.php", {
-//                     method: "POST",
-//                     body: formData
-//                 })
-//                 .then(function (data) {
-//                     console.log(data);
-//                     console.log('Отправлено');
-//                     form.reset();
-//                 });
-//         }
-//     });
+// const showMessage = () => {
+//     popupMessege.classList.add('modal__thanks--active');
+//     popupForm.classList.add('modal__form--hide');
 // }
 
-// validateForms('.form', {
-//     email: {
-//         required: true,
-//         email: true
-//     },
-//     fio: {
-//         required: true
-//     },
-//     tel: {
-//         required: true
-//     }
-// });
+// popupForm.addEventListener('submit', (evt) => {
+//     // отменяем отправку
+//     evt.preventDefault();
 
+//     showMessage();
+// })
 
+// let popupWrapperSecond = document.querySelector('.second-modal');
+// let popupSecond = popupWrapperSecond.querySelector(".modal__content");
+// let popupFormSecond = popupSecond.querySelector('.modal__form');
+// let popupMessegeSecond = popupSecond.querySelector('.modal__thanks')
 
-let popupWrapperFirst = document.querySelector('.first-modal');
-let popup = popupWrapperFirst.querySelector('.modal__content');
-let popupForm = popup.querySelector('.modal__form');
-let popupMessege = popup.querySelector('.modal__thanks');
+// const showMessageSecond = () => {
+//     popupMessegeSecond.classList.add('modal__thanks--active');
+//     popupFormSecond.classList.add('modal__form--hide');
 
-const showMessage = () => {
-    popupMessege.classList.add('modal__thanks--active');
-    popupForm.classList.add('modal__form--hide');
+// }
+
+// popupFormSecond.addEventListener('submit', (evt) => {
+//     // отменяем отправку
+//     evt.preventDefault();
+
+//     showMessageSecond();
+// })
+
+function validateForms(selector, rules) {
+  new window.JustValidate(selector, {
+    rules: rules,
+    submitHandler: function (form, values, ajax) {
+      console.log(form);
+      // let formData = new FormData(form);
+
+      // fetch("mail.php", {
+      //     method: "POST",
+      //     body: formData
+      // })
+      // .then(function(data) {
+      //     console.log(data);
+      //     console.log('Отправлено');
+      //     form.reset();
+      // });
+    },
+  });
 }
 
-
-
-popupForm.addEventListener('submit', (evt) => {
-    // отменяем отправку
-    evt.preventDefault();
-
-    showMessage();
-})
-
-let popupWrapperSecond = document.querySelector('.second-modal');
-let popupSecond = popupWrapperSecond.querySelector(".modal__content");
-let popupFormSecond = popupSecond.querySelector('.modal__form');
-let popupMessegeSecond = popupSecond.querySelector('.modal__thanks')
-
-
-
-const showMessageSecond = () => {
-    popupMessegeSecond.classList.add('modal__thanks--active');
-    popupFormSecond.classList.add('modal__form--hide');
-
-}
-
-popupFormSecond.addEventListener('submit', (evt) => {
-    // отменяем отправку
-    evt.preventDefault();
-
-    showMessageSecond();
-})
+validateForms(".modal__form-1", {
+  email: {
+    required: true,
+    email: true,
+  },
+  fio: {
+    required: true,
+  },
+  tel: {
+    required: true,
+  },
+});
